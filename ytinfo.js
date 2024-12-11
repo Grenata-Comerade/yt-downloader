f99272bd4d89c4fb79ab806fe45da30687e7a8be
Grenata <cybergarium@gmail.com>
2024-12-11 07:53:31 +0700

    Update ytinfo.js

--- ytinfo.js
+++ ytinfo.js
@@ -1,1 +1,20 @@
+const ytdl = require("@distube/ytdl-core")
 
+async function getBasicInfo(url) {
+  return ytdl.getBasicInfo(url);
+}
+async function getTitle(url) {
+  const info = await ytdl.getBasicInfo(url);
+  return info;
+}
+
+async function getKeywords(url) {
+  const {keywords} = (await ytdl.getBasicInfo(url)).videoDetails;
+  return keywords;
+}
+
+(async () => {
+  const url = ""; // paste your YouTube Link
+  const info = await getTitle(url);
+  console.log(info);
+})();
